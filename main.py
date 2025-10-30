4935c2532bfd5e460871d9d22720a6fdb40ebf54
shuta1123 <shutay1123@yahoo.co.jp>
2025-10-30 15:28:39 +0900

    lecture05_01_x24099.pyの作成

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.lecture05_01 import lecture05_01
+from src.lecture05_01_x24099 import lecture05_01_x24099
 
 if __name__ == "__main__":
-    lecture05_01()
+    # lecture05_01()
+    lecture05_01_x24099()
